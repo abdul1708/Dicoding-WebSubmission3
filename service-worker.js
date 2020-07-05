e9b77836bcaad7fb344b035ea66687732684f497
rian-gustandi <rian.gustandi17@gmail.com>
2020-07-05 18:36:24 +0700

    Change - manifest.json | service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Menyimpan Aset ke Cache
-const CACHE_NAME = "firstpwa-v8";
+const CACHE_NAME = "firstpwa-v1";
 var urlsToCache = [
   "/",
   "/nav.html",
@@ -10,7 +10,11 @@ var urlsToCache = [
   "/pages/gallery.html",
   "/css/materialize.min.css",
   "/js/materialize.min.js",
-  "/js/nav.js"
+  "/js/nav.js",
+  "/assets/img/moba1200.png",
+  "/assets/img/moba512.png",
+  "/assets/img/moba725.png",
+  "/manifest.json"
 ];
 
 self.addEventListener("install", function (event) {
@@ -22,16 +26,16 @@ self.addEventListener("install", function (event) {
 });
 
 // Menggunakan Aset dari Cache
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches
       .match(event.request, { cacheName: CACHE_NAME })
-      .then(function(response) {
+      .then(function (response) {
         if (response) {
           console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
           return response;
         }
- 
+
         console.log(
           "ServiceWorker: Memuat aset dari server: ",
           event.request.url
